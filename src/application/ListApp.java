afb6abc09ffebff73d17c7bb22845d928d1e3d85
tallen95 <32628544+tallen95@users.noreply.github.com>
2018-02-08 19:52:57 -0500

    Add files via upload

--- src/application/ListApp.java
+++ src/application/ListApp.java
@@ -1,34 +1,40 @@
 package application;
 	
-import java.io.IOException;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
-import view.ListController;
 
 
 public class ListApp extends Application {
 	@Override
 	public void start(Stage primaryStage) 
-	throws IOException {
+	throws Exception {
 		FXMLLoader loader = new FXMLLoader();   
 		loader.setLocation(
-				getClass().getResource("/view/List.fxml"));
-		AnchorPane root = (AnchorPane)loader.load();
+				getClass().getClassLoader().getResource("/view/List.fxml"));
+		Parent root = loader.load();
 
-
-		ListController listController = loader.getController();
-		listController.start(primaryStage);
-
-		Scene scene = new Scene(root, 200, 300);
+		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
+		primaryStage.setTitle("Song Library");
+		primaryStage.setResizable(false);
 		primaryStage.show(); 
+		
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	@Override
+	public void stop(){
+        LibModel model = LibModel.getTheModel();
+        //
+        model.storeToFile();
 	}
 }
